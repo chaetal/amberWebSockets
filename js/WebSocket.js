18eb1eb8925b258a8eed1ad33431419a8546b94d
chaetal <chaetal@gmail.com>
2014-01-29 08:34:36 +0400

    Initial commit

---
+++ js/WebSocket.js
@@ -0,0 +1,22 @@
+define("chaetal_websocket/WebSocket", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_core/Kernel-Objects"], function(smalltalk,nil,_st){
+smalltalk.addPackage('WebSocket');
+smalltalk.packages["WebSocket"].transport = {"type":"amd","amdNamespace":"chaetal_websocket"};
+
+smalltalk.addClass('WebSocketTests', smalltalk.Object, [], 'WebSocket');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "testSuccessfulConnection",
+category: 'not yet classified',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+return self}, function($ctx1) {$ctx1.fill(self,"testSuccessfulConnection",{},smalltalk.WebSocketTests)})},
+args: [],
+source: "testSuccessfulConnection",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.WebSocketTests);
+
+
+});
