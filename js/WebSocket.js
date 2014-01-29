4ea91fb84b411eadc35bee83066a518cc20d2bc6
chaetal <chaetal@gmail.com>
2014-01-29 09:14:27 +0400

    WebSocket connection is being established, but test is not completed yet.

--- js/WebSocket.js
+++ js/WebSocket.js
@@ -1,20 +1,35 @@
-define("chaetal_websocket/WebSocket", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_core/Kernel-Objects"], function(smalltalk,nil,_st){
+define("chaetal_websocket/WebSocket", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_core/SUnit"], function(smalltalk,nil,_st){
 smalltalk.addPackage('WebSocket');
 smalltalk.packages["WebSocket"].transport = {"type":"amd","amdNamespace":"chaetal_websocket"};
 
-smalltalk.addClass('WebSocketTests', smalltalk.Object, [], 'WebSocket');
+smalltalk.addClass('WebSocketTests', smalltalk.TestCase, [], 'WebSocket');
 smalltalk.addMethod(
 smalltalk.method({
 selector: "testSuccessfulConnection",
-category: 'not yet classified',
+category: 'tests',
 fn: function (){
 var self=this;
+var socket;
+function $NativeFunction(){return smalltalk.NativeFunction||(typeof NativeFunction=="undefined"?nil:NativeFunction)}
+function $Transcript(){return smalltalk.Transcript||(typeof Transcript=="undefined"?nil:Transcript)}
 return smalltalk.withContext(function($ctx1) { 
-return self}, function($ctx1) {$ctx1.fill(self,"testSuccessfulConnection",{},smalltalk.WebSocketTests)})},
+var $1,$2;
+socket=_st($NativeFunction())._constructor_value_("WebSocket","ws://echo.websocket.org");
+_st(socket)._onopen_((function(event){
+return smalltalk.withContext(function($ctx2) {
+_st($Transcript())._cr();
+$ctx2.sendIdx["cr"]=1;
+$1=_st($Transcript())._show_("Connected!!!");
+$ctx2.sendIdx["show:"]=1;
+return $1;
+}, function($ctx2) {$ctx2.fillBlock({event:event},$ctx1,1)})}));
+_st($Transcript())._cr();
+$2=_st($Transcript())._show_("Main thread finished");
+return self}, function($ctx1) {$ctx1.fill(self,"testSuccessfulConnection",{socket:socket},smalltalk.WebSocketTests)})},
 args: [],
-source: "testSuccessfulConnection",
-messageSends: [],
-referencedClasses: []
+source: "testSuccessfulConnection\x0a  | socket |\x0a  socket := NativeFunction constructor: 'WebSocket' value: 'ws://echo.websocket.org'.\x0a  socket onopen: [:event | Transcript cr; show: 'Connected!!!'].\x0a  Transcript cr; show: 'Main thread finished'.",
+messageSends: ["constructor:value:", "onopen:", "cr", "show:"],
+referencedClasses: ["NativeFunction", "Transcript"]
 }),
 smalltalk.WebSocketTests);
 
